4e8bdf6c60d35a965c902275ed27a246ab7bdbe9
mlorrayne <lorrayne_pego@yahoo.com.br>
2023-05-14 22:21:32 -0300

    Implementação de testes unitários para a classe de manipulação de exceções.

---
+++ src/test/java/com/lmoura/api/controller/exceptions/ControllerExceptionsHandlerTest.java
@@ -0,0 +1,58 @@
+package com.lmoura.api.controller.exceptions;
+
+import com.lmoura.api.service.exceptions.DataIntegrityViolationException;
+import com.lmoura.api.service.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.logging.Handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ControllerExceptionsHandlerTest {
+
+    @InjectMocks
+    private ControllerExceptionsHandler exceptionsHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionsHandler
+                .objectNotFound(new ObjectNotFoundException("Objeto não encontrado"),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Objeto não encontrado", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionsHandler
+                .dataIntegrityViolationException(new DataIntegrityViolationException("E-mail já cadastrado."),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("E-mail já cadastrado.", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+    }
+}
